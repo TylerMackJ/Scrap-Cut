752d2c09f74cdc3855ff0dc97745f146b955d601
Tyler Johnson <tylermackj@gmail.com>
2022-08-26 14:24:42 -0500

    Reformat and fixed taken enum

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
 #[derive(Clone, PartialEq)]
 enum Square { Free, Taken(u8), Scrap, Good }
 
+impl Square {
+    fn is_taken(&self) -> bool {
+        matches!(self, Square::Taken(_))
+    }
+}
+
 struct Sheet {
     width: usize,
-    height: usize
+    height: usize,
+}
+
+struct Grid<T> {
+    grid: Vec<T>,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl<T> Grid<T> {
+    fn new(width: usize, height: usize, default: T) -> Grid<T> {
+        Grid<T> {
+            grid: vec![width * height],
+            width = width,
+            height = height,
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> &mut T {
+        &mut self.grid[x + (y * self.width)]
+    }
 }
 
 struct Vec2 {
     x: f32,
-    y: f32
+    y: f32,
 }
 
 struct Line2D {
     start: Vec2,
-    end: Vec2
+    end: Vec2,
 }
 
 fn main() {
@@ -24,12 +50,7 @@ fn main() {
 
     // Create Grid
     let resolution: usize = 2;
-    let mut grid: Vec<Vec<Square>> = vec![Vec::new(); sheet.height / resolution];
-
-    for mut row in grid
-    {
-        row = vec![Square::Free; sheet.width / resolution];
-    }
+    let mut grid: Grid<Square> = Grid::new(sheet.width / resolution, sheet.height / resolution, Square::Free);
 
     /*
     // Place shapes into grid
@@ -48,21 +69,21 @@ fn main() {
     */
     
     // Find all the Square::Scrap and Square::Good squares
-    for i in 0..grid.len()
+    for x in 0..grid.width;
     {
-        for j in 0..grid[0].len()
+        for y in 0..grid.height
         {
             // All Square::Free change
-            if grid[i][j] == Square::Free
+            if grid.get(x, y) == Square::Free
             {
                 // Square::Good if 2 orthogonal squares are not Square::Taken else Square::Scrap
-                if (grid[i + 1][j] != Square::Taken(_) || grid[i - 1][j] != Square::Taken(_)) && (grid[i][j + 1] != Square::Taken(_) || grid[i][j - 1] != Square::Taken(_))
+                if (!grid.get(x, y).is_taken() || !grid.get(x - 1, y).is_taken()) && (!grid.get(x, y + 1).is_taken() || !grid.get(x, y - 1).is_taken())
                 {
-                    grid[i][j] == Square::Good;
+                    grid.get(x, y) = Square::Good;
                 }
                 else
                 {
-                    grid[i][j] == Square::Scrap;
+                    grid.get(x, y) = Square::Scrap;
                 }
             }
         }
@@ -70,15 +91,15 @@ fn main() {
 
     // Find all the cuts
     //let mut cuts = Vec::new<Line2D>();
-    for i in 0..grid.len()
+    for x in 0..grid.width
     {
-        for j in 0..grid[0].len()
+        for y in 0..grid.height
         {
             // If Square::Scrap is next to Square::Good and all by itself
                 // Change to Square::Good
 
             // Find Square::Good-Square::Scrap cuts
-            if grid[i][j] == Square::Scrap && (grid[i + 1][j] == Square::Good || grid[i - 1][j] == Square::Good || grid[i][j + 1] == Square::Good || grid[i][j - 1] == Square::Good)
+            if grid.get(x, y) == Square::Scrap && (grid.get(x + 1, y) == Square::Good || grid.get(x - 1, y) == Square::Good || grid.get(x, y + 1) == Square::Good || grid.get(x, y - 1) == Square::Good)
             {
                 // Find each shape that has a Square::Taken touching the current square
                     // Make cut from middle of current square to the closest point of each shape
@@ -86,18 +107,18 @@ fn main() {
 
             // Find Taken-Good-Wall Cuts
             // Check top and bottom
-            if i == 0 || i == grid.len() - 1
+            if x == 0 || x == grid.width - 1
             {
-                if grid[i][j] == Square::Taken(_) && grid[i][j + 1] == Square::Good
+                if grid.get(x, y).is_taken() && grid.(x, y + 1)) == Square::Good
                 {
                     // Cut where the shape belonging to the current square is closest to the wall
                 }
             }
 
             // Check left and right
-            if j == 0 || j == grid[0].len() - 1
+            if y == 0 || y == grid.height - 1
             {
-                if grid[i][j] == Square::Taken(_) && grid[i + 1][j] == Square::Good
+                if grid.get(x, y).is_taken() && grid.get(x + 1, y) == Square::Good
                 {
                     // Cut where the shape belonging to the current square is closest to the wall
                 }
@@ -108,20 +129,20 @@ fn main() {
             let mut goodCount = 0;
             let mut xTaken: Option<Square> = None;
             let mut yTaken: Option<Square> = None;
-            for k in 0..2
+            for i in 0..2
             {
-                for l in 0..2
+                for j in 0..2
                 {
-                    match grid[i + k][j + l]
+                    match grid.get(x + i, y + j)
                     {
                         Square::Taken(_) => {
                             if xTaken.is_none()
                             {
-                                xTaken = grid[i + k][j + l];
+                                xTaken = Some(grid.get(x + i, y + j));
                             }
                             else
                             {
-                                yTaken = grid[i + k][j + l];
+                                yTaken = Some(grid.get(x + i, y + j));
                             }
                         },
                         Square::Good => goodCount += 1,
