65948a87e60c137b184b2822c1752766db53577e
Tyler Johnson <tylermackj@gmail.com>
2022-08-26 14:47:36 -0500

    eq

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ impl Square {
     }
 }
 
-impl<'a> PartialEq<Square> for &mut 'a Square {
+impl<'a> PartialEq<Square> for &'a mut Square {
     fn eq(&mut self, other: &Square) -> bool {
         *self == other
     }
