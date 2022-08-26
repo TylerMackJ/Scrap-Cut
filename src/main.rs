2a1952a3fd989839e87bc4aeedb3b3e4872b2fa0
Tyler Johnson <tylermackj@gmail.com>
2022-08-26 15:53:30 -0500

    Rustify

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ use std::ops::{Index, IndexMut};
 #[derive(Copy, Clone, PartialEq)]
 enum Square { Free, Taken(u8), Scrap, Good }
 
-impl Square {
+trait IsTaken {
+    fn is_taken(&self) -> bool;
+}
+
+impl IsTaken for Square {
     fn is_taken(&self) -> bool {
         matches!(self, Square::Taken(_))
     }
 }
 
-impl Option<Square> {
+impl IsTaken for Option<&Square> {
     fn is_taken(&self) -> bool {
-        return match self {
-            Some(s) => matches!(s, Square::Taken(_)),
-            None => false,
-        }
+        matches!(self, Some(Square::Taken(_)))
     }
 }
+
 
 impl PartialEq<Square> for &Square {
     fn eq(&self, other: &Square) -> bool {
@@ -26,10 +28,7 @@ impl PartialEq<Square> for &Square {
 
 impl PartialEq<Square> for Option<&Square> {
     fn eq(&self, other: &Square) -> bool {
-        return match self {
-            Some(s) => *s == other,
-            None => false,
-        }
+        matches!(*s, Some(other))
     }
 }
 
