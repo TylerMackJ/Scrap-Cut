c53db168241f63c9e853d43febd43ec1eea2f331
Tyler Johnson <tylermackj@gmail.com>
2022-08-29 09:22:11 -0500

    Added f32 get variation

--- src/grid.rs
+++ src/grid.rs
@@ -4,6 +4,7 @@ pub struct Grid<T: Clone> {
     grid: Vec<T>,
     pub width: usize,
     pub height: usize,
+    resolution: usize,
 }
 
 impl<T: Clone> Index<(usize, usize)> for Grid<T> {
@@ -21,15 +22,16 @@ impl<T: Clone> IndexMut<(usize, usize)> for Grid<T> {
 }
 
 impl<T: Clone> Grid<T> {
-    pub fn new(width: usize, height: usize, default: T) -> Grid<T> {
+    pub fn new(width: usize, height: usize, default: T, resolution: usize) -> Grid<T> {
         Grid {
             grid: vec![default; width * height],
             width: width,
             height: height,
+            resolution: resolution,
         }
     }
 
-    pub fn get(&self, x: usize, y: usize) -> Option<&T> {
+    pub fn get<usize>(&self, x: usize, y: usize) -> Option<&T> {
         return if (0..self.width).contains(&x) && (0..self.height).contains(&y) {
             Some(&self[(x, y)])
         } else {
@@ -37,9 +39,25 @@ impl<T: Clone> Grid<T> {
         }
     }
 
-    pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
+    pub fn get<f32>(&self, x: f32, y: f32) -> Option<&T> {
+        return if (0..self.width).contains(&(x as usize)) && (0..self.height).contains(&(y as usize)) {
+            Some(&self[(x as usize, y as usize)])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_mut<usize>(&mut self, x: usize, y: usize) -> Option<&mut T> {
         return if (0..self.width).contains(&x) && (0..self.height).contains(&y) {
             Some(&mut self[(x, y)])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_mut<f32>(&mut self, x: f32, y: f32) -> Option<&mut T> {
+        return if (0..self.width).contains(&(x as usize)) && (0..self.height).contains(&(y as usize)) {
+            Some(&mut self[(x as usize, y as usize)])
         } else {
             None
         }
--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     // Create Grid
     let resolution: usize = 2;
-    let mut grid: Grid<Square> = Grid::new(sheet.width / resolution, sheet.height / resolution, Square::Free);
+    let mut grid: Grid<Square> = Grid::new(sheet.width / resolution, sheet.height / resolution, Square::Free, resolution);
 
     /*
     // Place shapes into grid
@@ -49,16 +49,16 @@ fn main() {
     for x in 0..grid.width {
         for y in 0..grid.height {
             // All Square::Free change
-            if grid.get(x, y) == Square::Free {
+            if grid.get<usize>(x, y) == Square::Free {
                 // Square::Good if 2 orthogonal squares are not Square::Taken else Square::Scrap
-                if (!grid.get(x, y).is_taken() || !grid.get(x - 1, y).is_taken()) && (!grid.get(x, y + 1).is_taken() || !grid.get(x, y - 1).is_taken()) {
-                    match grid.get_mut(x, y) {
+                if (!grid.get<usize>(x, y).is_taken() || !grid.get<usize>(x - 1, y).is_taken()) && (!grid.get<usize>(x, y + 1).is_taken() || !grid.get(x, y - 1).is_taken()) {
+                    match grid.get_mut<usize>(x, y) {
                         Some(s) => *s = Square::Good,
                         None => panic!(),
                     }
                 }
                 else {
-                    match grid.get_mut(x, y) {
+                    match grid.get_mut<usize>(x, y) {
                         Some(s) => *s = Square::Scrap,
                         None => panic!(),
                     }
@@ -75,7 +75,7 @@ fn main() {
                 // Change to Square::Good
 
             // Find Square::Good-Square::Scrap cuts
-            if grid.get(x, y) == Square::Scrap && (grid.get(x + 1, y) == Square::Good || grid.get(x - 1, y) == Square::Good || grid.get(x, y + 1) == Square::Good || grid.get(x, y - 1) == Square::Good) {
+            if grid.get<usize>(x, y) == Square::Scrap && (grid.get<usize>(x + 1, y) == Square::Good || grid.get<usize>(x - 1, y) == Square::Good || grid.get(x, y + 1) == Square::Good || grid.get(x, y - 1) == Square::Good) {
                 // Find each shape that has a Square::Taken touching the current square
                     // Make cut from middle of current square to the closest point of each shape
             }
@@ -83,14 +83,14 @@ fn main() {
             // Find Taken-Good-Wall Cuts
             // Check top and bottom
             if x == 0 || x == grid.width - 1 {
-                if grid.get(x, y).is_taken() && grid.get(x, y + 1) == Square::Good {
+                if grid.get<usize>(x, y).is_taken() && grid.get<usize>(x, y + 1) == Square::Good {
                     // Cut where the shape belonging to the current square is closest to the wall
                 }
             }
 
             // Check left and right
             if y == 0 || y == grid.height - 1 {
-                if grid.get(x, y).is_taken() && grid.get(x + 1, y) == Square::Good {
+                if grid.get<usize>(x, y).is_taken() && grid.get<usize>(x + 1, y) == Square::Good {
                     // Cut where the shape belonging to the current square is closest to the wall
                 }
             }
@@ -102,13 +102,13 @@ fn main() {
             let mut y_taken: Option<&Square> = None;
             for i in 0..2 {
                 for j in 0..2 {
-                    match grid.get(x + i, y + j) {
+                    match grid.get<usize>(x + i, y + j) {
                         Some(&Square::Taken(_)) => {
                             if x_taken.is_none() {
-                                x_taken = grid.get(x + i, y + j);
+                                x_taken = grid.get<usize>(x + i, y + j);
                             }
                             else {
-                                y_taken = grid.get(x + i, y + j);
+                                y_taken = grid.get<usize>(x + i, y + j);
                             }
                         },
                         Some(&Square::Good) => good_count += 1,
